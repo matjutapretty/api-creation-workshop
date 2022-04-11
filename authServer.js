985006a721935db9b5f18f104b3d3a00e0e72154
Pretty <mphefopretty1997@gmail.com>
2022-04-11 09:03:14 +0200

    added 24h expiry time

--- authServer.js
+++ authServer.js
@@ -40,7 +40,7 @@ app.post('/api/login', (req, res) => {
 })
 
 function generateAccessToken(user) {
-    return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '100s'})
+    return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '24h'})
 }
 
 
